e7f8dacb56483c427557a44e5919124fa8926f86
geomarts <netdark@hotmail.gr>
2021-04-04 23:07:17 +0300

    Tweaks

--- main.js
+++ main.js
@@ -1,29 +1,35 @@
 const tl = gsap.timeline({ paused: true });
+
 tl.to(".panels .panel:first-child, .panels .panel:last-child", {
     scaleY: 1,
     duration: 1,
 })
     .to(
-        ".panels .panel:nth-child(n+2), .panels .panel:nth-child(-n+5)",
-        { scaleY: 1, duration: 0.5, ease: "power2.out" },
+        ".panels .panel:not(:first-child):not(:last-child)",
+        { scaleY: 1 },
         "-=0.5"
     )
     .to(".panels .panel", {
         scaleY: 0,
         duration: 0.3,
         stagger: 0.05,
-        ease: "power2.out",
     })
-    .to(".panels", { autoAlpha: 0, duration: 0.3 })
+    .to(".panels", {
+        clipPath: "circle(0%)",
+        skewX: "0deg",
+        duration: 1,
+    })
     .to(
-        ".main-content",
+        ".page-main",
         {
             clipPath: "circle(100%)",
             duration: 1,
         },
-        "-=.3"
+        "-=0.3"
     );
+
 //console.log(tl.duration());
+
 window.addEventListener("load", function () {
     tl.play();
 });
